aadb99146e6b22f3fac739058295d9da30f359ae
TANAKA SORA <tanaka-so@gnavi.co.jp>
2019-09-13 19:18:12 +0900

    [mod] アドレス登録用のハッシュを作成

--- client/main.go
+++ client/main.go
@@ -1,36 +1,36 @@
+// client 
 package main
 
 import (
     "log"
     "net"
     //"time"
+    //"sync"
 )
 
 func main() {
     // UDPのエンドポイントを返す
-    //server_addr, err := net.ResolveUDPAddr("udp", "13.59.148.159:60000") // EC2
-    server_addr, err := net.ResolveUDPAddr("udp", ":60000")  // local
-
+    //serverAddr, err := net.ResolveUDPAddr("udp", "13.59.148.159:60000") // EC2
+    serverAddr, err := net.ResolveUDPAddr("udp", ":60000")  // local
     // エラー処理
     if err != nil {
        log.Fatal(err) 
     }
-    // 接続開始
-    conn, err := net.DialUDP("udp", nil, server_addr)
-    log.Println("dial up ...")
 
+    // 接続開始
+    conn, err := net.DialUDP("udp", nil, serverAddr)
+    log.Println("dial up ...", serverAddr.String())
     if err != nil {
        log.Fatal(err) 
     }
-    defer conn.Close()
 
     buffer := make([]byte, 128)
-    //other_addr := make(*net.UDPAddr, 8)
-    var other_addr *net.UDPAddr
+    //otherAddr := make(*net.UDPAddr, 8)
+    var otherAddr *net.UDPAddr
     
     for {
-        // 自分のアドレスを渡す
-        _, err := conn.Write([]byte(server_addr.String()))
+        // サーバに接続
+        _, err := conn.Write([]byte(serverAddr.String()))
         if err != nil {
             log.Fatal(err)
         }
@@ -38,9 +38,9 @@ func main() {
         // 相手のアドレスを受け取る
         n, err := conn.Read(buffer)
         if n > 0 {
-            other_addr, err = net.ResolveUDPAddr("udp", string(buffer[:n]))
+            otherAddr, err = net.ResolveUDPAddr("udp", string(buffer[:n]))
             log.Println(string(buffer[:n]), "buffer")
-            log.Println(other_addr, "other")
+            log.Println(otherAddr, "other")
             break
         }
     }
--- server/main.go
+++ server/main.go
@@ -1,37 +1,66 @@
+/*
+server
+    サーバ側
+*/
 package main
 
 import (
     "log"
     "net"
+    "sync"
 )
 
+// クライアントのアドレスを格納しておくハッシュ
+var (
+	clientAddrMap = make(map[string]*net.UDPAddr, 8)
+	mutex      sync.Mutex
+)
+
+// クライアントのアドレスを登録する
+func registClientAddr(addr *net.UDPAddr) {
+    mutex.Lock()
+    defer mutex.Unlock()
+    clientAddrMap[addr.String()] = addr
+}
 
 func main() {
     // UDPのエンドポイントを返す
-    server_addr, err := net.ResolveUDPAddr("udp", ":60000")
+    serverAddr, err := net.ResolveUDPAddr("udp", ":60000")
     if err != nil {
-       log.Fatal(err, "1")
+       log.Fatal(err, "1: resolve udp address error.")
     }
     
     // 接続確認
-    conn, err := net.ListenUDP("udp", server_addr)
+    conn, err := net.ListenUDP("udp", serverAddr)
     if err != nil {
-       log.Fatal(err)
+       log.Fatal(err, "2: listen udp error.")
     }
     defer conn.Close()
-
-    log.Println(conn.RemoteAddr(), "remote")
-    log.Println(conn.LocalAddr(), "local")
-
+    
     buffer := make([]byte, 512)
     for {
-        // 相手のアドレスを受け取る
-        n, client_addr, err := conn.ReadFromUDP(buffer)
+        // クライアントのアドレスを受け取る
+        _, clientAddr, err := conn.ReadFromUDP(buffer)
         if err != nil {
-            log.Fatal(err)
+            log.Fatal(err, "3: connect error")
         }
-        log.Println(client_addr.String(), "client")
-        log.Println(string(buffer[:n]), "buffer") // 受け取り
-        conn.WriteToUDP([]byte(server_addr.String()), client_addr)
+        // 接続クライアントのアドレス
+        // log.Println(conn.RemoteAddr(), "remote")
+        // サーバのアドレス 
+        // log.Println(conn.LocalAddr(), "local")
+
+        // 接続クライアントを登録
+        log.Println(clientAddrMap)
+        registClientAddr(clientAddr)
+        log.Println(clientAddrMap)
+
+        /* TODO
+        クライアントにアドレスを渡す
+        今はサーバのアドレス渡してるので
+        clientAddrMapから取り出して渡す
+        */
+        conn.WriteToUDP([]byte(serverAddr.String()), clientAddr)
     }
 }
+
+
